095aadbc19c75ca08230298e6920a6c068164e6d
cristina <cristina.outeda.rua@gmail.com>
2021-06-12 22:35:02 +0200

    unete first commit

---
+++ src/components/Unete/Unete.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { bussiness, pentagon } from "../../assets/Icons";
+import { Box } from "./Styles";
+
+export function Unete() {
+  return (
+    <div>
+      <Box>
+        <div>
+          <h1>Developers</h1>
+          <h2>Cuenta personal para entrar en el mundo dev</h2>
+        </div>
+      </Box>
+      <Box>
+        <div>
+          <h1>Business</h1>
+          <h2>Tienes o perteneces a una compañía</h2>
+        </div>
+      </Box>
+    </div>
+  );
+}
