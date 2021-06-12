1c84b7cb9f7b5e742ce18addcb54a2d7be476b94
cristina <cristina.outeda.rua@gmail.com>
2021-06-12 22:32:34 +0200

    header first commit

---
+++ src/components/Headers/Headers.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { StepsAuthentication } from "../../utils";
+import { Header, Volver, Steps, Login } from "./Style";
+import { Arrow } from "../../assets/Icons";
+
+export function Headers({ page, setPage, header }) {
+  return (
+    <div>
+      {page === 0 ? (
+        <Login>
+          <h2>Ya tienes cuenta?</h2>
+          <h3>Inicia Sesión</h3>
+        </Login>
+      ) : (
+        <Header>
+          <Volver onClick={() => setPage(page - 1)}>
+            <Arrow /> <span>Volver</span>
+          </Volver>
+          <Steps>
+            <h1>PASO 0{page}/03</h1>
+            <h2>{header}</h2>
+          </Steps>
+        </Header>
+      )}
+    </div>
+  );
+}
