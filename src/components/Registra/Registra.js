95e32ec1e44606de0baade4c73d8a8356bd4dd49
cristina <cristina.outeda.rua@gmail.com>
2021-06-12 23:30:15 +0200

    input tried in registra

--- src/components/Registra/Registra.js
+++ src/components/Registra/Registra.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import { InputComponent } from "../InputComponent";
 import { Square } from "./Style";
 
 export function Registra() {
   const [clicked, setClicked] = useState(false);
   return (
-    <Square clicked={clicked} onClick={() => setClicked(!clicked)}>
-      ✔
-    </Square>
+    <>
+      <Square clicked={clicked} onClick={() => setClicked(!clicked)}>
+        ✔
+      </Square>
+      <InputComponent id="cris" />
+    </>
   );
 }
