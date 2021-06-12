7d659016ffc199329a0c89c63c396e096efcdb2d
cristina <cristina.outeda.rua@gmail.com>
2021-06-12 22:38:15 +0200

    stepsAuth added

---
+++ src/utils/StepsAuthentication.js
@@ -0,0 +1,18 @@
+export const StepsAuthentication = [
+  {
+    title: "¡Únete a la comunidad!",
+    subtitle: "Para empezar, dinos que cuenta te gustaría abrir.",
+  },
+  {
+    header: "Persnal Info.",
+    title: "Registra tu cuenta individual",
+  },
+  {
+    header: "Localización",
+    title: "Complete Your Profile!",
+  },
+  {
+    header: "Verificación por tarjeta",
+    title: "Verifica tu perfil",
+  },
+];
