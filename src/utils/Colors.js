64232c984e715700e8a061137bc074647e769330
cristina <cristina.outeda.rua@gmail.com>
2021-06-12 22:37:01 +0200

    colors defined

---
+++ src/utils/Colors.js
@@ -0,0 +1,10 @@
+export const Theme = {
+  darkBody: "#494949",
+  black: "#000000",
+  lightPrimary: "#F5F9FF",
+  primary: "#4F934A",
+  middleBody: "#8692A6",
+  lightBody: "#BDBDBD",
+  white: "#FFFFFF",
+  label: "#696F79",
+};
