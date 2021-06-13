ef8c51591b210043e1a9fa0149d359a762f6154d
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 10:00:04 +0200

    modal styled (responsive)

--- src/components/Modal/Style.js
+++ src/components/Modal/Style.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
+import { IsPhone } from "../../utils";
 
 export const ModalContainer = styled.div`
+  -webkit-box-sizing: border-box; 
+  -moz-box-sizing: border-box; 
+  box-sizing: border-box; /
   width: 600px;
   display: flex;
   flex-direction: column;
@@ -15,6 +19,15 @@ export const ModalContainer = styled.div`
     0px 50.625px 34.875px rgba(0, 0, 0, 0.04725),
     0px 20px 13px rgba(0, 0, 0, 0.035),
     0px 4.375px 4.625px rgba(0, 0, 0, 0.02275);
+  @media (max-width: ${IsPhone}) {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100vw;
+    height: 100vh;
+    padding: 20px 0 0 0;
+    border-radius: 0;
+  }
 `;
 
 export const ModalBg = styled.div`
