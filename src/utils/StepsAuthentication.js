49e8667b7bf1822358e6cad69b89f32452b03fa1
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 10:02:43 +0200

    compoenents sent thought array

--- src/utils/StepsAuthentication.js
+++ src/utils/StepsAuthentication.js
@@ -1,18 +1,29 @@
+import { Unete, Registra, Complete, Verifica, Safe } from "../components";
+
 export const StepsAuthentication = [
   {
     title: "¡Únete a la comunidad!",
     subtitle: "Para empezar, dinos que cuenta te gustaría abrir.",
+    component: <Unete />,
   },
   {
     header: "Persnal Info.",
     title: "Registra tu cuenta individual",
+    component: <Registra />,
+    continuar: "Registrar cuenta",
   },
   {
     header: "Localización",
     title: "Complete Your Profile!",
+    component: <Complete />,
+    continuar: "Guardar y continuar",
+    safe: <Safe />,
   },
   {
     header: "Verificación por tarjeta",
     title: "Verifica tu perfil",
+    component: <Verifica />,
+    continuar: "Crear cuenta",
+    safe: <Safe />,
   },
 ];
