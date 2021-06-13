024290e130e61c0dffa53f76505dc41c6cd51bf8
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 09:58:26 +0200

    Complete component filled

--- src/components/Complete/Complete.js
+++ src/components/Complete/Complete.js
@@ -2,15 +2,33 @@ import React, { useState } from "react";
 import { InputComponent } from "../InputComponent";
 
 export function Complete() {
-  const [complete, setComplete] = useState("");
+  const [phone, setPhone] = useState("");
+  const [adress, setAdress] = useState("");
+  const [country, setCountry] = useState("");
   return (
-    <InputComponent
-      id="cris"
-      value={complete}
-      onChange={setComplete}
-      placeholder="hola cris"
-      label="phone"
-      isPhone
-    />
+    <>
+      <InputComponent
+        id="Phone"
+        value={phone}
+        onChange={setPhone}
+        label="Número de teléfono"
+        isPhone
+      />
+      <InputComponent
+        id="Adress"
+        value={adress}
+        onChange={setAdress}
+        placeholder="Introduce la dirección completa"
+        label="Dirección"
+      />
+      <InputComponent
+        id="Contraseña"
+        value={country}
+        onChange={setCountry}
+        placeholder="Selecciona uno"
+        label="País de residencia"
+        isCountry
+      />
+    </>
   );
 }
