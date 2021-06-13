751c7da787e9bc4e5a6816e1f6ce7d26a3bbeb3a
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 10:00:29 +0200

    registra component styled

--- src/components/Registra/Style.js
+++ src/components/Registra/Style.js
@@ -14,3 +14,22 @@ export const Square = styled.div`
   background: ${(props) => (props.clicked ? Theme.primary : Theme.white)};
   cursor: pointer;
 `;
+
+export const Acepto = styled.div`
+  display: flex;
+  width: 70%;
+  min-width: 300px;
+  justify-content: flex-start;
+  align-items: flex-end;
+  margin-top: 24px;
+  h1 {
+    margin-top: 0px;
+    margin-bottom: 0px;
+    margin-left: 15px;
+    color: ${Theme.middleBody};
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px !important;
+    line-height: 19px;
+  }
+`;
