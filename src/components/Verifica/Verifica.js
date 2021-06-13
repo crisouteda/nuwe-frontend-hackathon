041be7bd270fac0caefcbfa79d0cdb129f328b4a
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 10:01:51 +0200

    verifica component filled

--- src/components/Verifica/Verifica.js
+++ src/components/Verifica/Verifica.js
@@ -1,6 +1,25 @@
-import React from "react";
-import { Circle } from "./Style";
+import React, { useState } from "react";
+import { InputComponent } from "../InputComponent";
 
 export function Verifica() {
-  return <Circle>✔</Circle>;
+  const [card, setCard] = useState("");
+  const [code, setCode] = useState("");
+  return (
+    <>
+      <InputComponent
+        id="Card"
+        value={card}
+        onChange={setCard}
+        label="Número de tarjeta"
+        inputLength={12}
+      />
+      <InputComponent
+        id="Code"
+        value={code}
+        onChange={setCode}
+        label="Código secreto"
+        inputLength={3}
+      />
+    </>
+  );
 }
