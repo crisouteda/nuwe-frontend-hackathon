b657b3cda8438352982bc1191ff75c2268a03f1f
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 11:00:44 +0200

    properties to object

--- src/components/Unete/Unete.js
+++ src/components/Unete/Unete.js
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Bussiness, Developer, Pentagon } from "../../assets/Icons";
 // import { bussiness, pentagon } from "../../assets/Icons";
-import { Box } from "./Styles";
+import { Box, HideableSvg } from "./Styles";
 
-export function Unete() {
+const options = [
+  {
+    icon: Developer,
+    title: "Developers",
+    subtitle: "Cuenta personal para entrar en el mundo dev",
+  },
+  {
+    icon: Bussiness,
+    title: "Business",
+    subtitle: "Tienes o perteneces a una compañía",
+  },
+];
+
+export function Unete({ nextPage }) {
   return (
     <div>
-      <Box>
-        <div>
-          <h1>Developers</h1>
-          <h2>Cuenta personal para entrar en el mundo dev</h2>
-        </div>
-      </Box>
-      <Box>
-        <div>
-          <h1>Business</h1>
-          <h2>Tienes o perteneces a una compañía</h2>
-        </div>
-      </Box>
+      {options.map((o) => (
+        <AccountTypeOption o={o} key={o.title} nextPage={nextPage} />
+      ))}
     </div>
+  );
+}
+
+function AccountTypeOption({ o, nextPage }) {
+  const [hover, setHover] = useState(false);
+  return (
+    <Box
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onClick={() => {
+        nextPage(1);
+      }}
+    >
+      <Pentagon hover={!hover} />
+      <HideableSvg>
+        <o.icon
+          className="hide"
+          hover={hover}
+          style={{ position: "relative" }}
+        />
+      </HideableSvg>
+      <div style={{ marginLeft: 25 }}>
+        <h1>{o.title}</h1>
+        <h2>{o.subtitle}</h2>
+      </div>
+    </Box>
   );
 }
