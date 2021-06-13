0ead207637a5d1d2e9c5eb9327307f4c4c042a3e
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 10:00:55 +0200

    safe component created

---
+++ src/components/Safe/Safe.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeIcon } from "../../assets/Icons";
+import { Main } from "./Style";
+export function Safe() {
+  return (
+    <Main>
+      <SafeIcon />
+      <h1>Tu información es segura</h1>
+    </Main>
+  );
+}
