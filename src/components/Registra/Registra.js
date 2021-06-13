aa0bae757aa7237920c6d51f00487962c77e5694
cristina <cristina.outeda.rua@gmail.com>
2021-06-13 10:00:17 +0200

    registra component filled

--- src/components/Registra/Registra.js
+++ src/components/Registra/Registra.js
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
 import { InputComponent } from "../InputComponent";
-import { Square } from "./Style";
+import { Square, Acepto } from "./Style";
 
 export function Registra() {
   const [clicked, setClicked] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-      <Square clicked={clicked} onClick={() => setClicked(!clicked)}>
-        ✔
-      </Square>
-      <InputComponent id="cris" />
+      <InputComponent
+        id="Nombre"
+        value={nombre}
+        onChange={setNombre}
+        placeholder="Enter name"
+        label="Nombre completo*"
+      />
+      <InputComponent
+        id="Email"
+        value={email}
+        onChange={setEmail}
+        placeholder="Enter email adress"
+        label="Correo electrónico*"
+      />
+      <InputComponent
+        id="Contraseña"
+        value={password}
+        onChange={setPassword}
+        placeholder="Enter password"
+        label="Contraseña*"
+        isPassword
+      />
+      <Acepto>
+        <Square clicked={clicked} onClick={() => setClicked(!clicked)}>
+          ✔
+        </Square>
+        <h1>Acepto términos y condiciones</h1>
+      </Acepto>
     </>
   );
 }
